d6bf25ca1aa8cf0c8d2ada35f7f09bc98c5f3f3d
anshu1604 <engineeranshu1999@gmail.com>
2023-11-11 22:58:28 +0530

    play button added

--- SplitApp/components/organisms/Landing/styles.ts
+++ SplitApp/components/organisms/Landing/styles.ts
@@ -2,7 +2,7 @@ import {StyleSheet} from 'react-native';
 import {Colors} from '../../../resources/styleConstants';
 
 const landingStyles = StyleSheet.create({
-  main: {flex: 1, backgroundColor: Colors.dark},
+  main: {flex: 1, backgroundColor: Colors.dark, alignItems: 'center'},
 });
 
 export default landingStyles;
